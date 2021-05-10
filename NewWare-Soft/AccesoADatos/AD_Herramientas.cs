045ccd8ed6ff9defdddb4e39acb94e33c8446417
84235_Juan_Rencini <juanirencin@gmail.com>
2021-05-10 13:04:43 -0300

    Creacion de Clase y AD Herramienta

---
+++ NewWare-Soft/AccesoADatos/AD_Herramientas.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewWare_Soft.AccesoADatos
+{
+    public class AD_Herramientas
+    {
+        public static bool ValidarHerramienta(string nombreHerramienta, string descripcion)
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            try
+            {
+                bool resultado = false;
+                SqlCommand cmd = new SqlCommand();
+
+                string consulta = "SELECT * FROM herramientas WHERE NombreHerramienta like @nombreHerr AND Descripcion like @descrip";
+
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@nombreHerr", nombreHerramienta);
+                cmd.Parameters.AddWithValue("@descrip", descripcion);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+
+                DataTable tabla = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+                if (tabla.Rows.Count == 1)
+                {
+                    resultado = true;
+                }
+                else
+                {
+                    resultado = false;
+                }
+                return resultado;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
---
+++ NewWare-Soft/Forms/HerramientasForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace NewWare_Soft.Forms
+{
+    partial class HerramientasForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // HerramientasForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "HerramientasForm";
+            this.Text = "HerramientasForm";
+            this.Load += new System.EventHandler(this.HerramientasForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
