4735681fb0a46847252c305fa3b73d02c46fb517
81937 Lautaro Caceres <jlcaceresc01@gmail.com>
2021-05-12 16:53:56 -0300

    ...

--- NewWare-Soft/AccesoADatos/AD_FPago.cs
+++ NewWare-Soft/AccesoADatos/AD_FPago.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,40 @@ namespace NewWare_Soft.AccesoADatos
 {
     class AD_FPago
     {
+        public static DataTable ObtenerTiposPago()
+        {
+
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+
+                string consulta = "SELECT * FROM tipos_formas_pago";
+
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+
+                DataTable tabla = new DataTable();
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+                return tabla;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
     }
 }
--- NewWare-Soft/Entidades/FormaPago.cs
+++ NewWare-Soft/Entidades/FormaPago.cs
@@ -8,5 +8,36 @@ namespace NewWare_Soft.Entidades
 {
     class FormaPago
     {
+        private int NroFactura;
+        private int IdFormaPago;
+        private string Descripcion;
+        private int Monto;
+
+
+        public FormaPago()
+        {
+
+        }
+
+        public int NroFacturaPersona
+        {
+            get => NroFactura;
+            set => NroFactura = value;
+        }
+        public int IdFormaPagoPersona
+        {
+            get => IdFormaPago;
+            set => IdFormaPago = value;
+        }
+        public string DescripcionPersona
+        {
+            get => Descripcion;
+            set => Descripcion = value;
+        }
+        public int MontoPersona
+        {
+            get => Monto;
+            set => Monto = value;
+        }
     }
 }
