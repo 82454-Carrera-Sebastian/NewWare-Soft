10db3f7aff7fd3c453efcd1d1495d7c3825daa4d
facuraba <80785557+facuraba@users.noreply.github.com>
2021-05-12 02:58:44 -0300

    Commit cliente

---
+++ NewWare-Soft/Forms/AltaCliente.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace NewWare_Soft.Forms
+{
+    partial class AltaCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_limpiarCampos = new System.Windows.Forms.Button();
+            this.button_agregarCliente = new System.Windows.Forms.Button();
+            this.textBox_barrio = new System.Windows.Forms.TextBox();
+            this.label_barrio = new System.Windows.Forms.Label();
+            this.textBox_nroCalle = new System.Windows.Forms.TextBox();
+            this.label_calle = new System.Windows.Forms.Label();
+            this.label_nroCalle = new System.Windows.Forms.Label();
+            this.textBox_nombreCalle = new System.Windows.Forms.TextBox();
+            this.textBox_Email = new System.Windows.Forms.TextBox();
+            this.label_telefono = new System.Windows.Forms.Label();
+            this.label_Email = new System.Windows.Forms.Label();
+            this.textBox_telefono = new System.Windows.Forms.TextBox();
+            this.textBox_apellido = new System.Windows.Forms.TextBox();
+            this.label_nombre = new System.Windows.Forms.Label();
+            this.label_apellido = new System.Windows.Forms.Label();
+            this.textBox_nombre = new System.Windows.Forms.TextBox();
+            this.label_titulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(428, 16);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "no podra modificar su email posteriormente";
+            // 
+            // button_limpiarCampos
+            // 
+            this.button_limpiarCampos.Font = new System.Drawing.Font("Lucida Console", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_limpiarCampos.Location = new System.Drawing.Point(35, 555);
+            this.button_limpiarCampos.Name = "button_limpiarCampos";
+            this.button_limpiarCampos.Size = new System.Drawing.Size(122, 60);
+            this.button_limpiarCampos.TabIndex = 46;
+            this.button_limpiarCampos.Text = "Limpiar campos";
+            this.button_limpiarCampos.UseVisualStyleBackColor = true;
+            // 
+            // button_agregarCliente
+            // 
+            this.button_agregarCliente.Font = new System.Drawing.Font("Lucida Console", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_agregarCliente.Location = new System.Drawing.Point(422, 555);
+            this.button_agregarCliente.Name = "button_agregarCliente";
+            this.button_agregarCliente.Size = new System.Drawing.Size(122, 60);
+            this.button_agregarCliente.TabIndex = 45;
+            this.button_agregarCliente.Text = "Agregar";
+            this.button_agregarCliente.UseVisualStyleBackColor = true;
+            // 
+            // textBox_barrio
+            // 
+            this.textBox_barrio.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_barrio.Location = new System.Drawing.Point(35, 504);
+            this.textBox_barrio.Name = "textBox_barrio";
+            this.textBox_barrio.Size = new System.Drawing.Size(509, 31);
+            this.textBox_barrio.TabIndex = 43;
+            // 
+            // label_barrio
+            // 
+            this.label_barrio.AutoSize = true;
+            this.label_barrio.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_barrio.Location = new System.Drawing.Point(31, 477);
+            this.label_barrio.Name = "label_barrio";
+            this.label_barrio.Size = new System.Drawing.Size(94, 24);
+            this.label_barrio.TabIndex = 44;
+            this.label_barrio.Text = "Barrio";
+            // 
+            // textBox_nroCalle
+            // 
+            this.textBox_nroCalle.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_nroCalle.Location = new System.Drawing.Point(33, 431);
+            this.textBox_nroCalle.Name = "textBox_nroCalle";
+            this.textBox_nroCalle.Size = new System.Drawing.Size(511, 31);
+            this.textBox_nroCalle.TabIndex = 40;
+            // 
+            // label_calle
+            // 
+            this.label_calle.AutoSize = true;
+            this.label_calle.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_calle.Location = new System.Drawing.Point(28, 333);
+            this.label_calle.Name = "label_calle";
+            this.label_calle.Size = new System.Drawing.Size(80, 24);
+            this.label_calle.TabIndex = 42;
+            this.label_calle.Text = "Calle";
+            // 
+            // label_nroCalle
+            // 
+            this.label_nroCalle.AutoSize = true;
+            this.label_nroCalle.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_nroCalle.Location = new System.Drawing.Point(29, 404);
+            this.label_nroCalle.Name = "label_nroCalle";
+            this.label_nroCalle.Size = new System.Drawing.Size(178, 24);
+            this.label_nroCalle.TabIndex = 41;
+            this.label_nroCalle.Text = "Nro de calle";
+            // 
+            // textBox_nombreCalle
+            // 
+            this.textBox_nombreCalle.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_nombreCalle.Location = new System.Drawing.Point(33, 360);
+            this.textBox_nombreCalle.Name = "textBox_nombreCalle";
+            this.textBox_nombreCalle.Size = new System.Drawing.Size(511, 31);
+            this.textBox_nombreCalle.TabIndex = 39;
+            // 
+            // textBox_Email
+            // 
+            this.textBox_Email.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Email.Location = new System.Drawing.Point(33, 289);
+            this.textBox_Email.Name = "textBox_Email";
+            this.textBox_Email.Size = new System.Drawing.Size(511, 31);
+            this.textBox_Email.TabIndex = 34;
+            // 
+            // label_telefono
+            // 
+            this.label_telefono.AutoSize = true;
+            this.label_telefono.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_telefono.Location = new System.Drawing.Point(28, 191);
+            this.label_telefono.Name = "label_telefono";
+            this.label_telefono.Size = new System.Drawing.Size(122, 24);
+            this.label_telefono.TabIndex = 38;
+            this.label_telefono.Text = "Telefono";
+            // 
+            // label_Email
+            // 
+            this.label_Email.AutoSize = true;
+            this.label_Email.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Email.Location = new System.Drawing.Point(29, 262);
+            this.label_Email.Name = "label_Email";
+            this.label_Email.Size = new System.Drawing.Size(80, 24);
+            this.label_Email.TabIndex = 37;
+            this.label_Email.Text = "Email";
+            // 
+            // textBox_telefono
+            // 
+            this.textBox_telefono.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_telefono.Location = new System.Drawing.Point(33, 218);
+            this.textBox_telefono.Name = "textBox_telefono";
+            this.textBox_telefono.Size = new System.Drawing.Size(511, 31);
+            this.textBox_telefono.TabIndex = 33;
+            // 
+            // textBox_apellido
+            // 
+            this.textBox_apellido.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_apellido.Location = new System.Drawing.Point(33, 149);
+            this.textBox_apellido.Name = "textBox_apellido";
+            this.textBox_apellido.Size = new System.Drawing.Size(511, 31);
+            this.textBox_apellido.TabIndex = 32;
+            // 
+            // label_nombre
+            // 
+            this.label_nombre.AutoSize = true;
+            this.label_nombre.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_nombre.Location = new System.Drawing.Point(28, 51);
+            this.label_nombre.Name = "label_nombre";
+            this.label_nombre.Size = new System.Drawing.Size(94, 24);
+            this.label_nombre.TabIndex = 36;
+            this.label_nombre.Text = "Nombre";
+            // 
+            // label_apellido
+            // 
+            this.label_apellido.AutoSize = true;
+            this.label_apellido.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_apellido.Location = new System.Drawing.Point(29, 122);
+            this.label_apellido.Name = "label_apellido";
+            this.label_apellido.Size = new System.Drawing.Size(122, 24);
+            this.label_apellido.TabIndex = 35;
+            this.label_apellido.Text = "Apellido";
+            // 
+            // textBox_nombre
+            // 
+            this.textBox_nombre.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_nombre.Location = new System.Drawing.Point(33, 78);
+            this.textBox_nombre.Name = "textBox_nombre";
+            this.textBox_nombre.Size = new System.Drawing.Size(511, 31);
+            this.textBox_nombre.TabIndex = 31;
+            // 
+            // label_titulo
+            // 
+            this.label_titulo.AutoSize = true;
+            this.label_titulo.Font = new System.Drawing.Font("Lucida Console", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_titulo.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label_titulo.Location = new System.Drawing.Point(28, 12);
+            this.label_titulo.Name = "label_titulo";
+            this.label_titulo.Size = new System.Drawing.Size(369, 26);
+            this.label_titulo.TabIndex = 30;
+            this.label_titulo.Text = "Agregar nuevo cliente";
+            // 
+            // AltaCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 625);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_limpiarCampos);
+            this.Controls.Add(this.button_agregarCliente);
+            this.Controls.Add(this.textBox_barrio);
+            this.Controls.Add(this.label_barrio);
+            this.Controls.Add(this.textBox_nroCalle);
+            this.Controls.Add(this.label_calle);
+            this.Controls.Add(this.label_nroCalle);
+            this.Controls.Add(this.textBox_nombreCalle);
+            this.Controls.Add(this.textBox_Email);
+            this.Controls.Add(this.label_telefono);
+            this.Controls.Add(this.label_Email);
+            this.Controls.Add(this.textBox_telefono);
+            this.Controls.Add(this.textBox_apellido);
+            this.Controls.Add(this.label_nombre);
+            this.Controls.Add(this.label_apellido);
+            this.Controls.Add(this.textBox_nombre);
+            this.Controls.Add(this.label_titulo);
+            this.Name = "AltaCliente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alta Cliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_limpiarCampos;
+        private System.Windows.Forms.Button button_agregarCliente;
+        private System.Windows.Forms.TextBox textBox_barrio;
+        private System.Windows.Forms.Label label_barrio;
+        private System.Windows.Forms.TextBox textBox_nroCalle;
+        private System.Windows.Forms.Label label_calle;
+        private System.Windows.Forms.Label label_nroCalle;
+        private System.Windows.Forms.TextBox textBox_nombreCalle;
+        private System.Windows.Forms.TextBox textBox_Email;
+        private System.Windows.Forms.Label label_telefono;
+        private System.Windows.Forms.Label label_Email;
+        private System.Windows.Forms.TextBox textBox_telefono;
+        private System.Windows.Forms.TextBox textBox_apellido;
+        private System.Windows.Forms.Label label_nombre;
+        private System.Windows.Forms.Label label_apellido;
+        private System.Windows.Forms.TextBox textBox_nombre;
+        private System.Windows.Forms.Label label_titulo;
+    }
+}
---
+++ NewWare-Soft/Forms/BajaCliente.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace NewWare_Soft.Forms
+{
+    partial class BajaCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox_confirmacion = new System.Windows.Forms.GroupBox();
+            this.button_SI = new System.Windows.Forms.Button();
+            this.button_NO = new System.Windows.Forms.Button();
+            this.label_consulta = new System.Windows.Forms.Label();
+            this.button_Eliminar = new System.Windows.Forms.Button();
+            this.textBox_Email = new System.Windows.Forms.TextBox();
+            this.label_Seleccion = new System.Windows.Forms.Label();
+            this.dataGridView_Clientes = new System.Windows.Forms.DataGridView();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre_cliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellido_cliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox_confirmacion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Clientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox_confirmacion
+            // 
+            this.groupBox_confirmacion.Controls.Add(this.button_SI);
+            this.groupBox_confirmacion.Controls.Add(this.button_NO);
+            this.groupBox_confirmacion.Controls.Add(this.label_consulta);
+            this.groupBox_confirmacion.Enabled = false;
+            this.groupBox_confirmacion.Location = new System.Drawing.Point(243, 104);
+            this.groupBox_confirmacion.Name = "groupBox_confirmacion";
+            this.groupBox_confirmacion.Size = new System.Drawing.Size(360, 136);
+            this.groupBox_confirmacion.TabIndex = 13;
+            this.groupBox_confirmacion.TabStop = false;
+            this.groupBox_confirmacion.Text = "Confirmacion";
+            this.groupBox_confirmacion.Visible = false;
+            // 
+            // button_SI
+            // 
+            this.button_SI.Location = new System.Drawing.Point(279, 71);
+            this.button_SI.Name = "button_SI";
+            this.button_SI.Size = new System.Drawing.Size(75, 32);
+            this.button_SI.TabIndex = 2;
+            this.button_SI.Text = "SI";
+            this.button_SI.UseVisualStyleBackColor = true;
+            // 
+            // button_NO
+            // 
+            this.button_NO.Location = new System.Drawing.Point(10, 71);
+            this.button_NO.Name = "button_NO";
+            this.button_NO.Size = new System.Drawing.Size(75, 32);
+            this.button_NO.TabIndex = 1;
+            this.button_NO.Text = "NO";
+            this.button_NO.UseVisualStyleBackColor = true;
+            // 
+            // label_consulta
+            // 
+            this.label_consulta.AutoSize = true;
+            this.label_consulta.Location = new System.Drawing.Point(6, 22);
+            this.label_consulta.Name = "label_consulta";
+            this.label_consulta.Size = new System.Drawing.Size(348, 20);
+            this.label_consulta.TabIndex = 0;
+            this.label_consulta.Text = "¿Esta seguro que desea eliminar a este cliente?";
+            // 
+            // button_Eliminar
+            // 
+            this.button_Eliminar.Enabled = false;
+            this.button_Eliminar.Location = new System.Drawing.Point(619, 419);
+            this.button_Eliminar.Name = "button_Eliminar";
+            this.button_Eliminar.Size = new System.Drawing.Size(114, 46);
+            this.button_Eliminar.TabIndex = 12;
+            this.button_Eliminar.Text = "Eliminar";
+            this.button_Eliminar.UseVisualStyleBackColor = true;
+            // 
+            // textBox_Email
+            // 
+            this.textBox_Email.Enabled = false;
+            this.textBox_Email.Location = new System.Drawing.Point(339, 368);
+            this.textBox_Email.Name = "textBox_Email";
+            this.textBox_Email.Size = new System.Drawing.Size(394, 26);
+            this.textBox_Email.TabIndex = 11;
+            // 
+            // label_Seleccion
+            // 
+            this.label_Seleccion.AutoSize = true;
+            this.label_Seleccion.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Seleccion.Location = new System.Drawing.Point(68, 371);
+            this.label_Seleccion.Name = "label_Seleccion";
+            this.label_Seleccion.Size = new System.Drawing.Size(276, 24);
+            this.label_Seleccion.TabIndex = 10;
+            this.label_Seleccion.Text = "Ha seleccionado a: ";
+            // 
+            // dataGridView_Clientes
+            // 
+            this.dataGridView_Clientes.AllowUserToAddRows = false;
+            this.dataGridView_Clientes.AllowUserToDeleteRows = false;
+            this.dataGridView_Clientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Clientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Email,
+            this.nombre_cliente,
+            this.apellido_cliente});
+            this.dataGridView_Clientes.Location = new System.Drawing.Point(68, 12);
+            this.dataGridView_Clientes.Name = "dataGridView_Clientes";
+            this.dataGridView_Clientes.ReadOnly = true;
+            this.dataGridView_Clientes.RowHeadersWidth = 62;
+            this.dataGridView_Clientes.RowTemplate.Height = 28;
+            this.dataGridView_Clientes.Size = new System.Drawing.Size(665, 339);
+            this.dataGridView_Clientes.TabIndex = 9;
+            // 
+            // Email
+            // 
+            this.Email.DataPropertyName = "Email";
+            this.Email.HeaderText = "Email";
+            this.Email.MinimumWidth = 8;
+            this.Email.Name = "Email";
+            this.Email.ReadOnly = true;
+            this.Email.Width = 200;
+            // 
+            // nombre_cliente
+            // 
+            this.nombre_cliente.DataPropertyName = "Nombre";
+            this.nombre_cliente.HeaderText = "Nombre";
+            this.nombre_cliente.MinimumWidth = 8;
+            this.nombre_cliente.Name = "nombre_cliente";
+            this.nombre_cliente.ReadOnly = true;
+            this.nombre_cliente.Width = 200;
+            // 
+            // apellido_cliente
+            // 
+            this.apellido_cliente.DataPropertyName = "Apellido";
+            this.apellido_cliente.HeaderText = "Apellido";
+            this.apellido_cliente.MinimumWidth = 8;
+            this.apellido_cliente.Name = "apellido_cliente";
+            this.apellido_cliente.ReadOnly = true;
+            this.apellido_cliente.Width = 200;
+            // 
+            // BAJAcliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 498);
+            this.Controls.Add(this.groupBox_confirmacion);
+            this.Controls.Add(this.button_Eliminar);
+            this.Controls.Add(this.textBox_Email);
+            this.Controls.Add(this.label_Seleccion);
+            this.Controls.Add(this.dataGridView_Clientes);
+            this.Name = "BajaCliente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Baja de cliente";
+            this.groupBox_confirmacion.ResumeLayout(false);
+            this.groupBox_confirmacion.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Clientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_confirmacion;
+        private System.Windows.Forms.Button button_SI;
+        private System.Windows.Forms.Button button_NO;
+        private System.Windows.Forms.Label label_consulta;
+        private System.Windows.Forms.Button button_Eliminar;
+        private System.Windows.Forms.TextBox textBox_Email;
+        private System.Windows.Forms.Label label_Seleccion;
+        private System.Windows.Forms.DataGridView dataGridView_Clientes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre_cliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellido_cliente;
+    }
+}
