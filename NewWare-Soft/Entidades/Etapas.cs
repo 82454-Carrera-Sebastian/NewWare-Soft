d8a90968fd6ee42b35ae1e36ffacb06f8c57a3e5
AgustinFavot <agustinfavot98@gmail.com>
2021-05-13 02:41:00 -0300

    ABM-Etapas-origin

---
+++ NewWare-Soft/Entidades/Etapas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewWare_Soft.Entidades
+{
+    class Etapas
+    {
+        private string nombre;
+
+        public Etapas()
+        { 
+
+        }
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+    }
+}
