869a4ba1f212640fa254945773a4af12b9f991f2
AgustinFavot <agustinfavot98@gmail.com>
2021-05-13 11:22:33 -0300

    ABM_ETAPAS

---
+++ NewWare-Soft/Forms/AltaEtapa.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace NewWare_Soft.Forms
+{
+    partial class AltaEtapa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dgvEtapas = new System.Windows.Forms.DataGridView();
+            this.NroEtapa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreEtapa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEtapas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(52, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(420, 91);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Alta Etapa";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(56, 153);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(137, 153);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(250, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(221, 201);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 3;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click_1);
+            // 
+            // dgvEtapas
+            // 
+            this.dgvEtapas.AllowUserToAddRows = false;
+            this.dgvEtapas.AllowUserToDeleteRows = false;
+            this.dgvEtapas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEtapas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NroEtapa,
+            this.NombreEtapa});
+            this.dgvEtapas.Location = new System.Drawing.Point(43, 251);
+            this.dgvEtapas.Name = "dgvEtapas";
+            this.dgvEtapas.ReadOnly = true;
+            this.dgvEtapas.Size = new System.Drawing.Size(443, 150);
+            this.dgvEtapas.TabIndex = 4;
+            // 
+            // NroEtapa
+            // 
+            this.NroEtapa.DataPropertyName = "IdEtapa";
+            this.NroEtapa.HeaderText = "Nro Etapa";
+            this.NroEtapa.Name = "NroEtapa";
+            this.NroEtapa.ReadOnly = true;
+            // 
+            // NombreEtapa
+            // 
+            this.NombreEtapa.DataPropertyName = "NombreEtapa";
+            this.NombreEtapa.HeaderText = "Nombre Etapa";
+            this.NombreEtapa.Name = "NombreEtapa";
+            this.NombreEtapa.ReadOnly = true;
+            this.NombreEtapa.Width = 300;
+            // 
+            // AltaEtapa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 450);
+            this.Controls.Add(this.dgvEtapas);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "AltaEtapa";
+            this.Text = "AltaEtapa";
+            this.Load += new System.EventHandler(this.AltaEtapa_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEtapas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dgvEtapas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NroEtapa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreEtapa;
+    }
+}
