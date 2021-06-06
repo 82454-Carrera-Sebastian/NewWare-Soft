e3e4725366be6f6d3cb2246d0f81298c1d42d7f9
81768-Sequione-Alejo <81641647+81768-Sequione-Alejo@users.noreply.github.com>
2021-06-06 19:35:50 -0300

    Listado de facturas y productos
    Listados hechos

---
+++ NewWare-Soft/AccesoADatos/AD_Facturas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewWare_Soft.AccesoADatos
+{
+    class AD_Facturas
+    {
+        public static DataTable obtenerListadoProductos()
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection connect = new SqlConnection(cadenaConexion);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "SELECT * FROM facturas";
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+                connect.Open();
+                cmd.Connection = connect;
+                DataTable tabla = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(tabla);
+                return tabla;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                connect.Close();
+            }
+        }
+    }
+}
