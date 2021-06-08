c6687a2f7e49074b50aaa173216778ac8f994870
David Wunderlin <daez-script@hotmail.com>
2021-06-08 19:53:31 -0300

    Listado de personal y de etapas 2
    Saludos kbros!

--- NewWare-Soft/Program.cs
+++ NewWare-Soft/Program.cs
@@ -1,5 +1,4 @@
-﻿using NewWare_Soft.Forms.Reportes;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,8 +16,7 @@ namespace NewWare_Soft
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Login());
-            Application.Run(new ReporteListadoEtapas());
+            Application.Run(new Login());
         }
     }
 }
