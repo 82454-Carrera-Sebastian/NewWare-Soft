c43e3745ad2dc0334fed7d90fc392c231f523f8e
Gonzalo Mainardi - 81985 <81985@sistemas.frc.utn.edu.ar>
2021-06-09 01:24:40 -0300

    Reportes Formas de Pago y Barrios

---
+++ NewWare-Soft/Forms/Reportes/ListadoBarrios.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Reporting.WinForms;
+using NewWare_Soft.AccesoADatos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewWare_Soft.Forms.Reportes
+{
+    public partial class ListadoBarrios : Form
+    {
+        public ListadoBarrios()
+        {
+            InitializeComponent();
+        }
+
+        private void ListadoBarrios_Load(object sender, EventArgs e)
+        {
+
+            this.reporte_barrios.RefreshReport();
+        }
+
+        private void reporte_barrios_Load(object sender, EventArgs e)
+        {
+            DataTable tabla = new DataTable();
+            tabla = AD_Barrios.obtenerDatosBarrios();
+
+            ReportDataSource ds = new ReportDataSource("DatosBarrios", tabla);
+
+            reporte_barrios.LocalReport.DataSources.Clear();
+            reporte_barrios.LocalReport.DataSources.Add(ds);
+            reporte_barrios.LocalReport.Refresh();
+        }
+    }
+}
---
+++ NewWare-Soft/Forms/Reportes/ListadoFormasDePago.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Reporting.WinForms;
+using NewWare_Soft.AccesoADatos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewWare_Soft.Forms.Reportes
+{
+    public partial class ListadoFormasDePago : Form
+    {
+        public ListadoFormasDePago()
+        {
+            InitializeComponent();
+        }
+
+        private void ListadoFormasDePago_Load(object sender, EventArgs e)
+        {
+
+            this.report_FormasdePago.RefreshReport();
+        }
+
+        private void report_FormasdePago_Load(object sender, EventArgs e)
+        {
+            DataTable tabla = new DataTable();
+            tabla = AD_FPago.ObtenerTiposPago();
+
+            ReportDataSource ds = new ReportDataSource("DatosFormaDePago", tabla);
+
+            report_FormasdePago.LocalReport.DataSources.Clear();
+            report_FormasdePago.LocalReport.DataSources.Add(ds);
+            report_FormasdePago.LocalReport.Refresh();
+
+        }
+    }
+}
