9b076eb6e30bb34bc46a4fcca0cdacb690ab4349
81937 Lautaro Caceres <jlcaceresc01@gmail.com>
2021-06-08 23:03:10 -0300

    reportes prodXcliente - proyectosActivos

---
+++ NewWare-Soft/AccesoADatos/AD_ProductosXCliente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewWare_Soft.AccesoADatos
+{
+    class AD_ProductosXCliente
+    {
+        public static DataTable obtenerListadoProductosXCliente()
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection connect = new SqlConnection(cadenaConexion);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+
+                string consulta = "SELECT * FROM productos_x_clientes";
+
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                connect.Open();
+                cmd.Connection = connect;
+
+                DataTable tabla = new DataTable();
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(tabla);
+                return tabla;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                connect.Close();
+            }
+        }
+    }
+}
