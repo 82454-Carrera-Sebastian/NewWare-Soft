4fd324ec9c1e4e3b534f9e95b863311dc50ce163
Gonzalo Mainardi - 81985 <81985@sistemas.frc.utn.edu.ar>
2021-06-14 00:03:06 -0300

    Reporte con filtro

--- NewWare-Soft/Forms/Reportes/ListadoFormasDePago.cs
+++ NewWare-Soft/Forms/Reportes/ListadoFormasDePago.cs
@@ -21,21 +21,137 @@ namespace NewWare_Soft.Forms.Reportes
 
         private void ListadoFormasDePago_Load(object sender, EventArgs e)
         {
-
             this.report_FormasdePago.RefreshReport();
         }
 
-        private void report_FormasdePago_Load(object sender, EventArgs e)
+        private void btnSiguiente_Click(object sender, EventArgs e)
         {
-            DataTable tabla = new DataTable();
-            tabla = AD_FPago.ObtenerTiposPago();
+            if (rdbNombre.Checked)
+            {
+                lblNombre.Visible = true;
+                txtNombre.Visible = true;
+                txtNombre.Enabled = true;
+                btnGenerarReporte2.Visible = true;
+                btnGenerarReporte2.Enabled = true;
+            }
+            if (rdb_ordenAlfabetico.Checked)
+            {
+                lblOrdenAlfabetico.Visible = true;
+                btnGenerarReporte2.Visible = true;
+                btnGenerarReporte2.Enabled = true;
+            }
 
-            ReportDataSource ds = new ReportDataSource("DatosFormaDePago", tabla);
+            if (rdb_reportenormal.Checked)
+            {
+                lblNormal.Visible = true;
+                btnGenerarReporte2.Visible = true;
+                btnGenerarReporte2.Enabled = true;
+            }
+        }
+        private DataTable buscarTablas()
+        {
+            if (rdbNombre.Checked)
+            {
+                DataTable tabla = new DataTable();
+                tabla = AD_FPago.nombreFormadePago(txtNombre.Text);
+                return tabla;
+            }
+            if (rdb_ordenAlfabetico.Checked)
+            {
+                DataTable tabla = new DataTable();
+                tabla = AD_FPago.ordenAlfabetico();
+                return tabla;
+            }
 
-            report_FormasdePago.LocalReport.DataSources.Clear();
-            report_FormasdePago.LocalReport.DataSources.Add(ds);
-            report_FormasdePago.LocalReport.Refresh();
+            if (rdb_reportenormal.Checked)
+            {
+                DataTable tabla = new DataTable();
+                tabla = AD_FPago.ObtenerTiposPago();
+                return tabla;
+            }
+
+            else
+            {
+                DataTable tabla = new DataTable();
+                tabla = AD_FPago.ObtenerTiposPago();
+                return tabla;
+            }
 
         }
+        private void armarReporte(DataTable tabla)
+        {
+            ReportDataSource ds = new ReportDataSource("DatosFormasDePago", tabla);
+            report_FormasdePago.LocalReport.DataSources.Clear();
+            report_FormasdePago.LocalReport.DataSources.Add(ds);
+            report_FormasdePago.RefreshReport();
+            report_FormasdePago.Visible = true;
+        }
+
+        private void btnGenerarReporte2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (rdbNombre.Checked)
+                {
+                    DataTable tabla = new DataTable();
+                    tabla = buscarTablas();
+                    if (tabla == null || tabla.Rows.Count == 0)
+                    {
+                        MessageBox.Show("El reporte esta vacio, intentelo de nuevo");
+                    }
+                    else
+                    {
+                        armarReporte(tabla);
+                        lblNombre.Visible = false;
+                        txtNombre.Text = " ";
+                        txtNombre.Visible = false;
+                        txtNombre.Enabled = false;
+                        btnGenerarReporte2.Enabled = false;
+                        btnGenerarReporte2.Visible = false;
+                    }
+                }
+
+                if (rdb_reportenormal.Checked)
+                {
+                    DataTable tabla = new DataTable();
+                    tabla = buscarTablas();
+                    if (tabla == null || tabla.Rows.Count == 0)
+                    {
+                        MessageBox.Show("El reporte esta vacio, intentelo de nuevo");
+                    }
+                    else
+                    {
+                        armarReporte(tabla);
+                        lblNormal.Visible = false;
+                        btnGenerarReporte2.Enabled = false;
+                        btnGenerarReporte2.Visible = false;
+                    }
+                }
+
+                if (rdb_ordenAlfabetico.Checked)
+                {
+                    DataTable tabla = new DataTable();
+                    tabla = buscarTablas();
+                    if (tabla == null || tabla.Rows.Count == 0)
+                    {
+                        MessageBox.Show("El reporte esta vacio, intentelo de nuevo");
+                    }
+                    else
+                    {
+                        armarReporte(tabla);
+                        lblOrdenAlfabetico.Visible = false;
+                        btnGenerarReporte2.Enabled = false;
+                        btnGenerarReporte2.Visible = false;
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ha ocurrido un ERROR inesperado. Intentelo de nuevo");
+            }
+           
+        }
+
     }
 }
