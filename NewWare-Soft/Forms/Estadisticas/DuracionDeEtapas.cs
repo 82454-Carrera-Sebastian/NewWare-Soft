25ba18da2dc95c1fb2af1651482c3cba539a7aea
Gonzalo Mainardi - 81985 <81985@sistemas.frc.utn.edu.ar>
2021-06-20 22:26:09 -0300

    Leve cambio

--- NewWare-Soft/Forms/Estadisticas/DuracionDeEtapas.cs
+++ NewWare-Soft/Forms/Estadisticas/DuracionDeEtapas.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Reporting.WinForms;
+using NewWare_Soft.AccesoADatos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +18,81 @@ namespace NewWare_Soft.Forms.Estadisticas
         {
             InitializeComponent();
         }
+        private void Estadistica_EtapasQueMasTardaron_Load(object sender, EventArgs e)
+        {
+
+            this.reporte_Etapas.RefreshReport();
+        }
+
+        private void btnSelecc_Click(object sender, EventArgs e)
+        {
+            if (rbTodas.Checked)
+            {
+                label1.Visible = true;
+                btnConfirmar.Visible = true;
+                btnConfirmar.Enabled = true;
+            }
+            if (rdbNombre.Checked)
+            {
+                label2.Visible = true;
+                txtNombre.Visible = true;
+                txtNombre.Enabled = true;
+                btnConfirmar.Visible = true;
+                btnConfirmar.Enabled = true;
+            }
+            if (rdbDuracionMayor.Checked)
+            {
+                label3.Visible = true;
+                txtNumero.Visible = true;
+                txtNumero.Enabled = true;
+                btnConfirmar.Visible = true;
+                btnConfirmar.Enabled = true;
+            }
+
+        }
+        private void armarReporte(DataTable tabla)
+        {
+            ReportDataSource ds = new ReportDataSource("EtapasMasTardaron", tabla);
+            reporte_Etapas.LocalReport.DataSources.Clear();
+            reporte_Etapas.LocalReport.DataSources.Add(ds);
+            reporte_Etapas.LocalReport.Refresh();
+            reporte_Etapas.RefreshReport();
+            reporte_Etapas.Visible = true;
+        }
+
+        private DataTable buscarTablas()
+        {
+            if (rbTodas.Checked)
+            {
+                DataTable tabla = new DataTable();
+                tabla = AD_Etapass.duracionEtapa();
+                return tabla;
+            }
+
+            if (rdbNombre.Checked)
+            {
+                DataTable tabla = new DataTable();
+                tabla = AD_Etapass.duracionEtapa();
+                return tabla;
+            }
+            else
+            {
+                DataTable tabla = new DataTable();
+                tabla = AD_Etapass.duracionEtapa();
+                return tabla;
+            }
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+
+            DataTable tabla = new DataTable();
+            tabla = buscarTablas();
+            armarReporte(tabla);
+
+        }
     }
+
 }
+    
+
